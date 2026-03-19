ec236c7e6626d925209d882a76453ccae6aec0a9
LIGNUM | Restauro e Engenharia <lignum.restauro@gmail.com>
2026-03-19 19:11:20 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<!-- ===================== script.js ===================== -->
+function scrollToProjects() {
+  document.getElementById('projects').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
